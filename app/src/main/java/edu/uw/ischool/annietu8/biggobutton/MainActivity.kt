b58d3009ddb7d2485f9115baf5cae4b48b465e93
annie-tu <annietu18@gmail.com>
2023-10-25 16:14:38 -0700

    functional button and counter

--- app/src/main/java/edu/uw/ischool/annietu8/biggobutton/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/annietu8/biggobutton/MainActivity.kt
@@ -2,10 +2,24 @@ package edu.uw.ischool.annietu8.biggobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private var clickCount: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            clickButton()
+        }
+    }
+
+    private fun clickButton() {
+        clickCount++
+        val newText = "You have pushed me $clickCount time${if (clickCount == 1) "" else "s"}!"
+        button.text = newText
     }
 }
